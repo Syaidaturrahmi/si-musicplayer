396a42de21d9f4213d61a6f0139b6a73e3100fc0
syaidaturrahmi <rahmisyaida@gmail.com>
2021-06-15 14:36:27 +0700

    Upload Pertama

---
+++ app/koneksi.php
@@ -0,0 +1,14 @@
+<?php
+namespace App;
+use PDO;
+class koneksi {
+    protected $db;
+    public function __construct()
+    {
+        try{
+            $this->db = new PDO("mysql:host=localhost;dbname=dbartis", "root", "");
+        } catch (PDOException $e) {
+            die ("Error : " . $e->getMessage());
+        }
+    }
+}
